3719c7914b679f5c9307001fe00e1f7ceed2569e
Karl Widborg Kortelainen <karl.widkor@gmail.com>
2023-09-10 15:36:29 +0200

    Testing dev env

---
+++ inluppar/inlupp1/hashtable.c
@@ -0,0 +1,13 @@
+#include "hashtable.h"
+#include <stddef.h>
+
+
+ioopm_hash_table_t *ioopm_hash_table_create(void) {
+    // TODO: Stub
+    return NULL;
+}
+
+void ioopm_hash_table_destroy(ioopm_hash_table_t *ht) {
+    // TODO: Stub
+    return;
+}
