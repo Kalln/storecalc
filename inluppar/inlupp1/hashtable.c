88c24f9a56ec9803c0cea637b0fe4c5415401974
Karl Widborg Kortelainen <karl.widkor@gmail.com>
2023-09-11 16:06:07 +0200

    hashtable insert

--- inluppar/inlupp1/hashtable.c
+++ inluppar/inlupp1/hashtable.c
@@ -29,9 +29,19 @@ void ioopm_hash_table_destroy(ioopm_hash_table_t *ht) {
 }
 
 void ioopm_hash_table_insert(ioopm_hash_table_t *ht, int key, char *value) {
-    //TODO: Stub
+    int bucket = key % 17; // Bucket to insert into.
+    entry_t *first_entry = ht->buckets[bucket]; // ptr to first entry in bucket.
+    entry_t *cursor = first_entry;
+
+    while (cursor != NULL) {
+        if(cursor->key == key) {
+            cursor->value = value;
+            return;
+        }
+    }
+    cursor = cursor->next;
 }
 
-char ioopm_hash_table_lookup(void) {
+char *ioopm_hash_table_lookup(ioopm_hash_table_t *ht, int key) {
     return NULL;
 }
