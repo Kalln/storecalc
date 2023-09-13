9d392d0a818f5814d5f8f7bdb79f205ff58c0ae7
wipa0947 <wipa0947@user.uu.se>
2023-09-13 13:44:11 +0200

    made abstractions for find key and create_entry

--- inluppar/inlupp1/hashtable.c
+++ inluppar/inlupp1/hashtable.c
@@ -1,6 +1,7 @@
 #include "hashtable.h"
 #include <stdlib.h>
 #include <stddef.h>
+#include <stdbool.h>
 
 // Karl Widborg Kortelainen & William Paradell
 
@@ -12,7 +13,8 @@ struct entry {
     entry_t *next;
 };
 
-struct hash_table {
+struct hash_table 
+{
     entry_t *buckets[17];
 };
 
@@ -23,23 +25,55 @@ ioopm_hash_table_t *ioopm_hash_table_create(void) {
     return result;
 }
 
-void ioopm_hash_table_destroy(ioopm_hash_table_t *ht) {
+void ioopm_hash_table_destroy(ioopm_hash_table_t *ht) 
+{
     free(ht);
     return;
 }
 
-void ioopm_hash_table_insert(ioopm_hash_table_t *ht, int key, char *value) {
-    int bucket = key % 17; // Bucket to insert into.
-    entry_t *first_entry = ht->buckets[bucket]; // ptr to first entry in bucket.
-    entry_t *cursor = first_entry;
+// Check if key exists in hashtable.
+static entry_t *find_entry_for_key(entry_t *bucket, int key)
+{
 
-    while (cursor != NULL) {
-        if(cursor->key == key) {
-            cursor->value = value;
-            return;
+    while (bucket != NULL)
+    {
+        if (bucket->key == key)
+        {
+            break;
         }
+        bucket = bucket->next;
     }
-    cursor = cursor->next;
+    return bucket;
+}
+
+// Creates an entry.
+static entry_t *create_entry(entry_t *first_entry, int key, char* val)
+{
+    entry_t *new_entry = calloc(1, sizeof(entry_t));
+    new_entry->key = key;
+    new_entry->value = val;
+    new_entry->next = first_entry;
+    return new_entry;
+}
+
+void ioopm_hash_table_insert(ioopm_hash_table_t *ht, int key, char *value) 
+{
+    int bucket = key % 17;
+    entry_t *existing_entry = find_entry_for_key(ht->buckets[bucket], key);
+
+    if (existing_entry != NULL) /// i.e., it exists
+    {
+        existing_entry->value = value;
+    }
+    else
+    {
+        /// Get a pointer to the first entry in the bucket
+        entry_t *first_entry = ht->buckets[bucket];
+        /// Create a new entry
+        entry_t *new_entry = create_entry(key, value, first_entry);
+        /// Make the new entry the first entry in the bucket
+        ht->buckets[bucket] = new_entry;
+    }
 }
 
 char *ioopm_hash_table_lookup(ioopm_hash_table_t *ht, int key) {
