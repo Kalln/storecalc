85d067b613a6aae96d07799e58960217d1636af4
wipa0947 <wipa0947@user.uu.se>
2023-09-25 13:06:38 +0200

    created a test file for linked_lists

---
+++ inluppar/inlupp1/linked_list_tests.c
@@ -0,0 +1,69 @@
+#include <CUnit/Basic.h>
+#include "linked_list.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int init_suite(void) 
+{
+  // Change this function if you want to do something *before* you
+  // run a test suite
+  return 0;
+}
+
+int clean_suite(void) 
+{
+  // Change this function if you want to do something *after* you
+  // run a test suite
+  return 0;
+}
+
+// Own tests here.
+
+
+// run the tests
+
+int main() {
+
+    //setup CUnit
+    if (CU_initialize_registry() != CUE_SUCCESS) 
+    {
+        return CU_get_error();
+    }
+
+
+    CU_pSuite linked_list_test = CU_add_suite("Linked list test", init_suite, clean_suite);
+    if (linked_list_test == NULL) 
+    {
+        // If the test suite could not be added, tear down CUnit and exit
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    // This is where we add the test functions to our test suite.
+    // For each call to CU_add_test we specify the test suite, the
+    // name or description of the test, and the function that runs
+    // the test in question. If you want to add another test, just
+    // copy a line below and change the information
+    if (
+        (CU_add_test(linked_list_test, "linked list create and destroy", test_create_destroy) == NULL) ||
+        0
+    ) 
+    {
+        // If adding any of the tests fails, we tear down CUnit and exit
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    // Set the running mode. Use CU_BRM_VERBOSE for maximum output.
+    // Use CU_BRM_NORMAL to only print errors and a summary
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+
+    // This is where the tests are actually run!
+    CU_basic_run_tests();
+
+    // Tear down CUnit before exiting
+    CU_cleanup_registry();
+
+    return CU_get_error();
+}
