c2b8c0a628a8aa91dd70747177f0d0ac9ad7070a
Karl Widborg Kortelainen <karl.widkor@gmail.com>
2023-09-25 16:24:21 +0200

    Added tests for linked list

--- inluppar/inlupp1/linked_list.c
+++ inluppar/inlupp1/linked_list.c
@@ -1,14 +1,24 @@
 #include "linked_list.h"
 #include <stdlib.h>
 
-typedef struct elem {
-    elem_t *next;
-    elem_t *prev;
-    int value;
-} elem_t;
+typedef struct elem elem_t;
 
-typedef struct list {
+
+struct elem 
+{
+    int val;
+    elem_t *next;
+};
+
+struct list 
+{
     elem_t *first;
     elem_t *last;
     int size;
-} list_t;
+};
+
+
+
+
+
+
--- inluppar/inlupp1/linked_list_tests.c
+++ inluppar/inlupp1/linked_list_tests.c
@@ -20,6 +20,86 @@ int clean_suite(void)
 
 // Own tests here.
 
+void test_create_empty_list(void)
+{
+    ioopm_list_t *lt = ioopm_linked_list_create();
+    CU_ASSERT_PTR_NOT_NULL(lt);
+    ioopm_linked_list_destroy(lt);
+}
+
+void test_append_link_list(void)
+{
+    ioopm_list_t *lt = ioopm_linked_list_create();
+    CU_ASSERT_FALSE( ioopm_linked_list_contains(lt, -1));
+    ioopm_linked_list_append(lt, -1);
+    CU_ASSERT_TRUE( ioopm_linked_list_contains(lt, -1));
+    ioopm_linked_list_destroy(lt);
+}
+
+void test_prepend_link_list(void)
+{
+    ioopm_list_t *lt = ioopm_linked_list_create();
+    
+    ioopm_linked_list_prepend(lt, 10);
+    // Hur kontrollera sista?
+    ioopm_linked_list_prepend(lt, 40);
+    // Hur kontrollera sista? 
+
+    ioopm_linked_list_destroy(lt);
+}
+
+void test_insert_link_list(void)
+{
+    ioopm_list_t *lt = ioopm_linked_list_create();
+    ioopm_linked_list_insert(lt, 0, 1);
+    CU_ASSERT_TRUE( ioopm_linked_list_contains(lt, -1));
+    ioopm_linked_list_destroy(lt);
+}
+
+void test_remove_elem_link_list(void)
+{
+    
+}
+
+void test_get_elem_by_index_link_list(void)
+{
+
+}
+
+void test_elem_contains_in_link_list(void)
+{
+
+}
+
+void test_link_list_size(void)
+{
+
+}
+
+void test_linked_list_empty(void)
+{
+
+}
+
+void test_clear_link_list(void)
+{
+
+}
+
+void test_linked_list_show_all(void)
+{
+
+}
+
+void test_linked_list_any(void)
+{
+
+}
+
+void test_link_list_apply_to_all(void)
+{
+    
+}
 
 // run the tests
 
@@ -46,7 +126,20 @@ int main() {
     // the test in question. If you want to add another test, just
     // copy a line below and change the information
     if (
-        (CU_add_test(linked_list_test, "linked list create and destroy", test_create_destroy) == NULL) ||
+        (CU_add_test(linked_list_test, "[ioopm_linked_list_[create / destroy]] linked list create and destroy", test_create_empty_list) == NULL) ||
+        (CU_add_test(linked_list_test, "[ioopm_linked_list_append] linked list append", test_append_link_list) == NULL) ||
+        (CU_add_test(linked_list_test, "[ioopm_linked_list_prepend]linked list prepend", test_prepend_link_list) == NULL) ||
+        (CU_add_test(linked_list_test, "[ioopm_linked_list_insert]linked list create and destroy", test_insert_link_list) == NULL) ||
+        (CU_add_test(linked_list_test, "[ioopm_linked_list_remove]linked list create and destroy", test_remove_elem_link_list) == NULL) ||
+        (CU_add_test(linked_list_test, "[ioopm_linked_list_get]linked list create and destroy", test_get_elem_by_index_link_list) == NULL) ||
+        (CU_add_test(linked_list_test, "[ioopm_linked_list_contains]linked list create and destroy", test_elem_contains_in_link_list) == NULL) ||
+        (CU_add_test(linked_list_test, "[ioopm_linked_list_size]linked list create and destroy", test_link_list_size) == NULL) ||
+        (CU_add_test(linked_list_test, "[ioopm_linked_list_is_empty] linked list create and destroy", test_linked_list_empty) == NULL) ||
+        (CU_add_test(linked_list_test, "[ioopm_linked_list_clear] linked list create and destroy", test_clear_link_list) == NULL) ||
+        (CU_add_test(linked_list_test, "[ioopm_linked_list_all] linked list create and destroy", test_linked_list_show_all) == NULL) ||
+        (CU_add_test(linked_list_test, "[ioopm_linked_list_any] linked list create and destroy", test_linked_list_any) == NULL) ||
+        (CU_add_test(linked_list_test, "[ioopm_linked_list_to_all] linked list create and destroy", test_link_list_apply_to_all) == NULL) ||
+
         0
     ) 
     {
