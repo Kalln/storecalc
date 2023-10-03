ee8f121aec610205178e55ce77ac5034ba909040
wipa0947 <wipa0947@user.uu.se>
2023-10-03 15:20:16 +0200

    index on master: a54cea0 works

--- inluppar/inlupp1/common.c
+++ inluppar/inlupp1/common.c
@@ -5,19 +5,15 @@
 
 bool int_eq(elem_t a, elem_t b)
 {
-    if (a.type == ELEM_V_PTR || b.type == ELEM_V_PTR)
-    {
-        return false;
-    }
-    return a.data.val == b.data.val;
+    return a.data.val == NULL && b.data.val == NULL ? true
+         : a.data.val == NULL || b.data.val == NULL ? false
+         : a.data.val == b.data.val;
 }
 
 bool str_eq(elem_t a, elem_t b)
 {
-    if (a.type == ELEM_V_PTR || b.type == ELEM_V_PTR)
-    {
-        return false;
-    }
-    return strcmp(a.data.str, b.data.str) == 0;
+    return a.data.str == NULL && b.data.str == NULL ? true
+         : a.data.str == NULL || b.data.str == NULL ? false
+         : strcmp(a.data.str, b.data.str) == 0;
 }
 
