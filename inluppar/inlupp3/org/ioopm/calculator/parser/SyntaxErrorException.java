9c569faa1dc68f25ceb9957c1fd041d4a2809be7
Kalle <karl.widkor@gmail.com>
2023-11-22 17:24:36 +0100

    Update SyntaxErrorException class

--- inluppar/inlupp3/org/ioopm/calculator/parser/SyntaxErrorException.java
+++ inluppar/inlupp3/org/ioopm/calculator/parser/SyntaxErrorException.java
@@ -1,9 +1,12 @@
 package org.ioopm.calculator.parser;
 
-public class SyntaxErrorException extends Exception {
+public class SyntaxErrorException extends RuntimeException {
+    public SyntaxErrorException() {
+        super();
+    }
 
     public SyntaxErrorException(String msg) {
         super(msg);
     }
-
+    
 }
