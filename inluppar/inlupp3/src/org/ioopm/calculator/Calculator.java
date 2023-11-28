d0e8c6994ca27e5ec5ef3a2a99b6507e4b2c84fb
Kalle <karl.widkor@gmail.com>
2023-11-28 14:32:38 +0100

    Add variables and track statistics

--- inluppar/inlupp3/src/org/ioopm/calculator/Calculator.java
+++ inluppar/inlupp3/src/org/ioopm/calculator/Calculator.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 import org.ioopm.calculator.ast.Environment;
 import org.ioopm.calculator.ast.IllegalExpressionException;
 import org.ioopm.calculator.ast.SymbolicExpression;
+import org.ioopm.calculator.ast.atom.Constant;
+import org.ioopm.calculator.ast.atom.Variable;
 import org.ioopm.calculator.ast.command.*;
 import org.ioopm.calculator.parser.CalculatorParser;
 import org.ioopm.calculator.parser.SyntaxErrorException;
@@ -14,6 +16,9 @@ public class Calculator {
 
     public static void main(String[] args) throws IOException, SyntaxErrorException, IllegalExpressionException {    
     CalculatorParser calcParser = new CalculatorParser();
+    int expressionEntered = 0;
+    int sucessfullyEvaluated = 0;
+    int fullyEvaluated = 0;
     Environment env = new Environment();
 
     Scanner scanner = new Scanner(System.in);
@@ -22,20 +27,31 @@ public class Calculator {
         System.out.print("> ");
         String input = scanner.nextLine();
         SymbolicExpression result = calcParser.parse(input, env);
-
+        expressionEntered++;
         if (result.isCommand()) {
-            if (result instanceof Quit) {
+            if (result instanceof Quit r) {
+                r.run(env, expressionEntered, sucessfullyEvaluated, fullyEvaluated);
                 break;
             }
-            else if (result instanceof Vars) {
-                System.out.println(env.toString());
+            else if (result instanceof Vars r) {
+                r.run(env, expressionEntered, sucessfullyEvaluated, fullyEvaluated);
                 continue;
-            } else if (result instanceof Clear) {
-                env.clear();
+            } else if (result instanceof Clear r) {
+                r.run(env, expressionEntered, sucessfullyEvaluated, fullyEvaluated);
+                continue;
+            }
+        }
+        else {
+            var evalRes = result.eval(env);
+            sucessfullyEvaluated++;
+            env.put(new Variable("ans"), evalRes);
+            System.out.println(evalRes);
+
+            if (evalRes instanceof Constant) {
+                fullyEvaluated++;
             }
         }
 
-        System.out.println(result.eval(env));
     }}
     
 }
