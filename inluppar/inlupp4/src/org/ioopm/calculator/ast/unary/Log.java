7a951f8cbefbb3aa507fda3ce08eb2c0c658a51f
Mauritz Sverredal <23345144+Marre3@users.noreply.github.com>
2023-12-11 16:21:22 +0100

    Fix formatting of error message to use decimal dot

--- inluppar/inlupp4/src/org/ioopm/calculator/ast/unary/Log.java
+++ inluppar/inlupp4/src/org/ioopm/calculator/ast/unary/Log.java
@@ -1,5 +1,7 @@
 package org.ioopm.calculator.ast.unary;
 
+import java.util.Locale;
+
 import org.ioopm.calculator.ast.Environment;
 import org.ioopm.calculator.ast.IllegalExpressionException;
 import org.ioopm.calculator.ast.SymbolicExpression;
@@ -19,14 +21,14 @@ public class Log extends Unary {
         SymbolicExpression arg = this.exp.eval(vars);
         if (arg.isConstant()) {
             double argumentVal = arg.getValue();
-            
+
             // when x <= 0, Log(x) is undefined.
             if (argumentVal <= 0) {
-                throw new IllegalExpressionException(String.format("Log(%f) is undefined", argumentVal));
-            } 
+                throw new IllegalExpressionException(String.format(Locale.US, "Log(%f) is undefined", argumentVal));
+            }
 
             return new Constant(Math.log(arg.getValue()));
-            
+
         }
         else {
             return new Log(arg);
