ce6257b1550466e752bc048f960db21b6a4da08e
Mauritz Sverredal <23345144+Marre3@users.noreply.github.com>
2023-12-15 18:14:48 +0100

    Remove trailing ", " from Environment::toString

--- inluppar/inlupp4/src/org/ioopm/calculator/ast/Environment.java
+++ inluppar/inlupp4/src/org/ioopm/calculator/ast/Environment.java
@@ -3,23 +3,16 @@ package org.ioopm.calculator.ast;
 import org.ioopm.calculator.ast.atom.Variable;
 
 import java.util.HashMap;
-import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class Environment extends HashMap<Variable, SymbolicExpression> {
-
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Variables: ");
-        TreeSet<Variable> vars = new TreeSet<>(this.keySet());
-
-        for (Variable v : vars) {
-            sb.append(v.getName());
-            sb.append(" = ");
-            sb.append(this.get(v));
-            sb.append(", ");
-        }
-
-        return sb.toString();
+        return "Variables: "
+            + this.keySet()
+                .stream()
+                .sorted()
+                .map(x -> x.getName() + " = " + this.get(x))
+                .collect(Collectors.joining(", "));
     }
 }
