193004c16ff7e62d3c67fff1efa70096d77ac8ba
Karl Widborg Kortelainen <karl.widkor@gmail.com>
2023-12-28 23:24:59 +0100

    Tests are not passing, some weird bug in the parser????

--- inluppar/inlupp4/ParserTests.java
+++ inluppar/inlupp4/ParserTests.java
@@ -1,5 +1,6 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.ioopm.calculator.ast.Conditional;
 import org.ioopm.calculator.ast.Environment;
 import org.ioopm.calculator.ast.IllegalExpressionException;
 import org.ioopm.calculator.ast.atom.Constant;
@@ -8,6 +9,11 @@ import org.ioopm.calculator.ast.atom.Variable;
 import org.ioopm.calculator.ast.binary.Addition;
 import org.ioopm.calculator.ast.binary.Assignment;
 import org.ioopm.calculator.ast.binary.Division;
+import org.ioopm.calculator.ast.binary.Equal;
+import org.ioopm.calculator.ast.binary.LessThan;
+import org.ioopm.calculator.ast.binary.LessThanEqual;
+import org.ioopm.calculator.ast.binary.MoreThan;
+import org.ioopm.calculator.ast.binary.MoreThanEqual;
 import org.ioopm.calculator.ast.binary.Multiplication;
 import org.ioopm.calculator.ast.binary.Subtraction;
 import org.ioopm.calculator.ast.command.Clear;
@@ -23,9 +29,6 @@ import org.ioopm.calculator.parser.CalculatorParser;
 import org.ioopm.calculator.parser.SyntaxErrorException;
 import org.junit.jupiter.api.Test;
 
-
-
-
 public class ParserTests {
     private Constant c1 = new Constant(42);
     private Constant c2 = new Constant(58);
@@ -38,9 +41,8 @@ public class ParserTests {
     private CalculatorParser parser = new CalculatorParser();
     private static Environment env = new Environment();
 
-
     @Test
-    void basicBinaryOperatorParse(){
+    void basicBinaryOperatorParse() {
         try {
             assertTrue(parser.parse("42", env).equals(c1));
             assertTrue(parser.parse("42 + 58", env).equals(add));
@@ -64,20 +66,17 @@ public class ParserTests {
         }
     }
 
-
     @Test
     void assignmentParse() {
         try {
             assertTrue(parser.parse("42 = x", env).equals(new Assignment(c1, var)));
             assertTrue(parser.parse("42 + 58 = x", env).equals(new Assignment(add, var)));
             assertEquals(
-                parser.parse("42 = x = y", env),
-                new Assignment(new Assignment(c1, new Variable("x")), new Variable("y"))
-            );
+                    parser.parse("42 = x = y", env),
+                    new Assignment(new Assignment(c1, new Variable("x")), new Variable("y")));
             assertEquals(
-                parser.parse("(42 = x) = y", env),
-                new Assignment(new Assignment(c1, new Variable("x")), new Variable("y"))
-            );
+                    parser.parse("(42 = x) = y", env),
+                    new Assignment(new Assignment(c1, new Variable("x")), new Variable("y")));
 
             assertThrows(SyntaxErrorException.class, () -> parser.parse("42 = (x = y)", env));
 
@@ -109,80 +108,62 @@ public class ParserTests {
 
         try {
             assertEquals(parser.parse("(42 + 58) * 42 - 58", env).toString(),
-                        new Multiplication(add, sub).toString());
+                    new Multiplication(add, sub).toString());
 
             assertEquals(parser.parse("42 + 58 * 42 - 58", env).toString(),
-                         new Addition(c1, new Subtraction(mul, c2)).toString());
+                    new Addition(c1, new Subtraction(mul, c2)).toString());
 
             // (Sin(3*x) * Cos(2*y)) / (a*a + b*b)
             var q1 = new Division(
-                new Multiplication(
-                    new Sin(new Multiplication(new Constant(3), new Variable("x"))),
-                    new Cos(new Multiplication(new Constant(2), new Variable("y")))
-                ),
-                new Addition(
-                    new Multiplication(new Variable("a"), new Variable("a")),
-                    new Multiplication(new Variable("b"), new Variable("b"))
-                )
-            );
+                    new Multiplication(
+                            new Sin(new Multiplication(new Constant(3), new Variable("x"))),
+                            new Cos(new Multiplication(new Constant(2), new Variable("y")))),
+                    new Addition(
+                            new Multiplication(new Variable("a"), new Variable("a")),
+                            new Multiplication(new Variable("b"), new Variable("b"))));
             // (4 * x * y) / (Cos(pi/4) + 2)
             var q2 = new Division(
-                new Multiplication(
-                    new Multiplication(new Constant(4), new Variable("x")),
-                    new Variable("y")
-                ),
-                new Addition(
-                    new Cos(
-                        new Division(
-                            new NamedConstant("pi", Math.PI),
-                            new Constant(4)
-                        )
-                    ),
-                    new Constant(2)
-                )
-            );
+                    new Multiplication(
+                            new Multiplication(new Constant(4), new Variable("x")),
+                            new Variable("y")),
+                    new Addition(
+                            new Cos(
+                                    new Division(
+                                            new NamedConstant("pi", Math.PI),
+                                            new Constant(4))),
+                            new Constant(2)));
             // Exp(-(2*z)) / Log(5*w)
             var q3 = new Division(
-                new Exp(new Negation(new Multiplication(new Constant(2), new Variable("z")))),
-                new Log(new Multiplication(new Constant(5), new Variable("w")))
-            );
+                    new Exp(new Negation(new Multiplication(new Constant(2), new Variable("z")))),
+                    new Log(new Multiplication(new Constant(5), new Variable("w"))));
             // Sin(x)*Sin(x) / Log(x*x + 1)
             var q4 = new Division(
-                new Multiplication(new Sin(new Variable("x")), new Sin(new Variable("x"))),
-                new Log(new Addition(new Multiplication(new Variable("x"), new Variable("x")), new Constant(1)))
-            );
+                    new Multiplication(new Sin(new Variable("x")), new Sin(new Variable("x"))),
+                    new Log(new Addition(new Multiplication(new Variable("x"), new Variable("x")), new Constant(1))));
             // (-(p*p) + 5*q) / Cos(r)
             var q5 = new Division(
-                new Addition(
-                    new Negation(new Multiplication(new Variable("p"), new Variable("p"))),
-                    new Multiplication(new Constant(5), new Variable("q"))
-                ),
-                new Cos(new Variable("r"))
-            );
+                    new Addition(
+                            new Negation(new Multiplication(new Variable("p"), new Variable("p"))),
+                            new Multiplication(new Constant(5), new Variable("q"))),
+                    new Cos(new Variable("r")));
             assertEquals(
-                parser.parse(
-                    (
-                        "(Sin(3*x) * Cos(2*y)) / (a*a + b*b)" // q1
-                        + " - (4 * x * y) / (Cos(pi/4) + 2)" // q2
-                        + " + Exp(-(2*z)) / Log(5*w)" // q3
-                        + " - Sin(x)*Sin(x) / Log(x*x + 1)" // q4
-                        + " + (-(p*p) + 5*q) / Cos(r)" // q5
-                    ),
-                    env
-                ),
-                new Addition(
-                    new Subtraction(
-                        new Addition(
-                            new Subtraction(
-                                q1,
-                                q2
+                    parser.parse(
+                            ("(Sin(3*x) * Cos(2*y)) / (a*a + b*b)" // q1
+                                    + " - (4 * x * y) / (Cos(pi/4) + 2)" // q2
+                                    + " + Exp(-(2*z)) / Log(5*w)" // q3
+                                    + " - Sin(x)*Sin(x) / Log(x*x + 1)" // q4
+                                    + " + (-(p*p) + 5*q) / Cos(r)" // q5
                             ),
-                            q3),
-                        q4
-                    ),
-                    q5
-                )
-            );
+                            env),
+                    new Addition(
+                            new Subtraction(
+                                    new Addition(
+                                            new Subtraction(
+                                                    q1,
+                                                    q2),
+                                            q3),
+                                    q4),
+                            q5));
 
         } catch (Exception e) {
             assertTrue(false);
@@ -209,28 +190,22 @@ public class ParserTests {
 
     @Test
     void unaryParse() {
-        try{
+        try {
             // Sin(1/2)
             assertTrue(parser.parse("Sin(1/2)", env).equals(
-                new Sin(
-                    new Division(
-                        new Constant(1),
-                        new Constant(2)
-                    )
-                )
-            ));
+                    new Sin(
+                            new Division(
+                                    new Constant(1),
+                                    new Constant(2)))));
 
             // Cos(1)
             assertTrue(parser.parse("Cos(1)", env).equals(new Cos(new Constant(1))));
 
             // Cos(Sin(1))
             assertTrue(parser.parse("Cos(Sin(1))", env).equals(
-                new Cos(
-                    new Sin(
-                        new Constant(1)
-                    )
-                )
-            ));
+                    new Cos(
+                            new Sin(
+                                    new Constant(1)))));
 
         } catch (Exception e) {
             assertTrue(false);
@@ -242,36 +217,27 @@ public class ParserTests {
     void scopeParse() {
         try {
             assertEquals(
-                parser.parse("{1 = x} + {1 = x}", env),
-                new Addition(
-                    new Scope(new Assignment(new Constant(1), new Variable("x"))),
-                    new Scope(new Assignment(new Constant(1), new Variable("x")))
-                )
-            );
+                    parser.parse("{1 = x} + {1 = x}", env),
+                    new Addition(
+                            new Scope(new Assignment(new Constant(1), new Variable("x"))),
+                            new Scope(new Assignment(new Constant(1), new Variable("x")))));
             assertEquals(
-                parser.parse("{{1 = x} = x}", env),
-                new Scope(new Assignment(new Scope(new Assignment(new Constant(1), new Variable("x"))), new Variable("x")))
-            );
+                    parser.parse("{{1 = x} = x}", env),
+                    new Scope(new Assignment(new Scope(new Assignment(new Constant(1), new Variable("x"))),
+                            new Variable("x"))));
             assertEquals(
-                parser.parse("(1 = x) + {(2 + x = x) + {3 + x = x}}", env),
-                new Addition(
-                    new Assignment(new Constant(1), new Variable("x")),
-                    new Scope(
-                        new Addition(
-                            new Assignment(
-                                new Addition(new Constant(2), new Variable("x")),
-                                new Variable("x")
-                            ),
+                    parser.parse("(1 = x) + {(2 + x = x) + {3 + x = x}}", env),
+                    new Addition(
+                            new Assignment(new Constant(1), new Variable("x")),
                             new Scope(
-                                new Assignment(
-                                    new Addition(new Constant(3), new Variable("x")),
-                                    new Variable("x")
-                                )
-                            )
-                        )
-                    )
-                )
-            );
+                                    new Addition(
+                                            new Assignment(
+                                                    new Addition(new Constant(2), new Variable("x")),
+                                                    new Variable("x")),
+                                            new Scope(
+                                                    new Assignment(
+                                                            new Addition(new Constant(3), new Variable("x")),
+                                                            new Variable("x")))))));
         } catch (Exception e) {
             System.out.println(e);
             assertTrue(false);
@@ -279,5 +245,41 @@ public class ParserTests {
 
     }
 
+    @Test
+    void testConditional() {
+        try {
+            var ifscope = new Scope(new Constant(42));
+            var elsescope = new Scope(new Constant(4711));
+
+            // if x < y { 42 } else { 4711 }
+            var cond1 = new LessThan(new Variable("x"), new Variable("y"));
+            var c1 = new Conditional(cond1, ifscope, elsescope);
+            assertEquals(parser.parse("if x < y { 42 } else { 4711 }", env).toString(), c1.toString());
+
+            //if x <= y { 42 } else { 4711 }
+            var cond2 = new LessThanEqual(new Variable("x"), new Variable("y"));
+            var c2 = new Conditional(cond2, ifscope, elsescope);
+            assertEquals(parser.parse("if x <= y { 42 } else { 4711 }", env).toString(), c2.toString());
+
+            // if x > y { 42 } else { 4711 }
+            var cond3 = new MoreThan(new Variable("x"), new Variable("y"));
+            var c3 = new Conditional(cond3, ifscope, elsescope);
+            assertEquals(parser.parse("if x > y { 42 } else { 4711 }", env).toString(), c3.toString());
+
+            // if x >= y { 42 } else { 4711 }
+            var cond4 = new MoreThanEqual(new Variable("x"), new Variable("y"));
+            var c4 = new Conditional(cond4, ifscope, elsescope);
+            assertEquals(parser.parse("if x >= y { 42 } else { 4711 }", env).toString(), c4.toString());
+
+            // if x == y { 42 } else { 4711 }
+            var cond5 = new Equal(new Variable("x"), new Variable("y"));
+            var c5 = new Conditional(cond5, ifscope, elsescope);
+            assertEquals(parser.parse("if x == y { 42 } else { 4711 }", env).toString(), c5.toString());
+
+        } catch (Exception e) {
+            System.out.println(e);
+            assertTrue(false);
+        }
+    }
 
 }
--- inluppar/inlupp4/src/org/ioopm/calculator/parser/CalculatorParser.java
+++ inluppar/inlupp4/src/org/ioopm/calculator/parser/CalculatorParser.java
@@ -35,19 +35,19 @@ public class CalculatorParser {
     // want to assign new meaning to is a valid name eg 3 = Quit
     // or 10 + x = L is not allowed
     private final ArrayList<String> unallowedVars = new ArrayList<String>(Arrays.asList(
-        "Quit",
-        "Vars",
-        "Clear",
-        "Cos",
-        "Sin",
-        "Exp",
-        "Log"
-    ));
+            "Quit",
+            "Vars",
+            "Clear",
+            "Cos",
+            "Sin",
+            "Exp",
+            "Log"));
 
     /**
      * Used to parse the inputted string by the Calculator program
+     * 
      * @param inputString the string used to parse
-     * @param vars the Environment in which the variables exist
+     * @param vars        the Environment in which the variables exist
      * @return a SymbolicExpression to be evaluated
      * @throws IOException by nextToken() if it reads invalid input
      */
@@ -63,19 +63,23 @@ public class CalculatorParser {
 
     /**
      * Checks wether the token read is a command or an assignment
+     * 
      * @return a SymbolicExpression to be evaluated
-     * @throws IOException by nextToken() if it reads invalid input
-     * @throws SyntaxErrorException if the token parsed cannot be turned into a valid expression
+     * @throws IOException          by nextToken() if it reads invalid input
+     * @throws SyntaxErrorException if the token parsed cannot be turned into a
+     *                              valid expression
      */
     private SymbolicExpression statement() throws IOException {
         SymbolicExpression result;
-        this.st.nextToken(); //kollar på nästa token som ligger på strömmen
+        this.st.nextToken(); // kollar på nästa token som ligger på strömmen
         if (this.st.ttype == StreamTokenizer.TT_EOF) {
             throw new SyntaxErrorException("Error: Expected an expression");
         }
 
         if (this.st.ttype == StreamTokenizer.TT_WORD) { // vilken typ det senaste tecken vi läste in hade.
-            if (this.st.sval.equals("Quit") || this.st.sval.equals("Vars") || this.st.sval.equals("Clear")) { // sval = string Variable
+            if (this.st.sval.equals("Quit") || this.st.sval.equals("Vars") || this.st.sval.equals("Clear")) { // sval =
+                                                                                                              // string
+                                                                                                              // Variable
                 result = command();
             } else {
                 result = assignment(); // går vidare med uttrycket.
@@ -94,9 +98,9 @@ public class CalculatorParser {
         return result;
     }
 
-
     /**
      * Checks what kind of command that should be returned
+     * 
      * @return an instance of Quit, Clear or Vars depending on the token parsed
      * @throws IOException by nextToken() if it reads invalid input
      */
@@ -110,14 +114,16 @@ public class CalculatorParser {
         }
     }
 
-
     /**
      * Checks wether the token read is an assignment between 2 expression and
      * descend into the right hand side of '='
+     * 
      * @return a SymbolicExpression to be evaluated
-     * @throws IOException by nextToken() if it reads invalid input
-     * @throws SyntaxErrorException if the token parsed cannot be turned into a valid expression,
-     *         the variable on rhs of '=' is a number or invalid variable
+     * @throws IOException          by nextToken() if it reads invalid input
+     * @throws SyntaxErrorException if the token parsed cannot be turned into a
+     *                              valid expression,
+     *                              the variable on rhs of '=' is a number or
+     *                              invalid variable
      */
     private SymbolicExpression assignment() throws IOException {
         SymbolicExpression result = expression();
@@ -127,7 +133,9 @@ public class CalculatorParser {
             if (this.st.ttype == StreamTokenizer.TT_NUMBER) {
                 throw new SyntaxErrorException("Error: Numbers cannot be used as a variable name");
             } else if (this.st.ttype != StreamTokenizer.TT_WORD) {
-                throw new SyntaxErrorException("Error: Not a valid assignment of a variable"); //this handles faulty inputs after the equal sign eg. 1 = (x etc
+                throw new SyntaxErrorException("Error: Not a valid assignment of a variable"); // this handles faulty
+                                                                                               // inputs after the equal
+                                                                                               // sign eg. 1 = (x etc
             } else {
                 if (this.st.sval.equals("ans")) {
                     throw new SyntaxErrorException("Error: ans cannot be redefined");
@@ -143,9 +151,12 @@ public class CalculatorParser {
 
     /**
      * Check if valid identifier for variable and return that if so
-     * @return a SymbolicExpression that is either a named constant or a new variable
-     * @throws IOException by nextToken() if it reads invalid input
-     * @throws IllegalExpressionException if you try to redefine a string that isn't allowed
+     * 
+     * @return a SymbolicExpression that is either a named constant or a new
+     *         variable
+     * @throws IOException                by nextToken() if it reads invalid input
+     * @throws IllegalExpressionException if you try to redefine a string that isn't
+     *                                    allowed
      */
     private SymbolicExpression identifier() throws IOException {
         SymbolicExpression result;
@@ -165,6 +176,7 @@ public class CalculatorParser {
     /**
      * Checks wether the token read is an addition or subtraction
      * and then continue on with the right hand side of operator
+     * 
      * @return a SymbolicExpression to be evaluated
      * @throws IOException by nextToken() if it reads invalid input
      */
@@ -189,6 +201,7 @@ public class CalculatorParser {
      * Checks wether the token read is an Multiplication or
      * Division and then continue on with the right hand side of
      * operator
+     * 
      * @return a SymbolicExpression to be evaluated
      * @throws IOException by nextToken() if it reads invalid input
      */
@@ -216,10 +229,12 @@ public class CalculatorParser {
      * operation is an unary operation and then continue on with
      * the right hand side of that operator else if it's a
      * number/identifier
+     * 
      * @return a SymbolicExpression to be evaluated
-     * @throws IOException by nextToken() if it reads invalid input
-     * @throws SyntaxErrorException if the token parsed cannot be turned into a valid expression,
-     *         missing right parantheses
+     * @throws IOException          by nextToken() if it reads invalid input
+     * @throws SyntaxErrorException if the token parsed cannot be turned into a
+     *                              valid expression,
+     *                              missing right parantheses
      */
     private SymbolicExpression primary() throws IOException {
         SymbolicExpression result;
@@ -241,15 +256,15 @@ public class CalculatorParser {
             result = unary();
         } else if (this.st.ttype == StreamTokenizer.TT_WORD) {
             if (st.sval.equals(SIN) ||
-                st.sval.equals(COS) ||
-                st.sval.equals(EXP) ||
-                st.sval.equals(NEG) ||
-                st.sval.equals(LOG)) {
+                    st.sval.equals(COS) ||
+                    st.sval.equals(EXP) ||
+                    st.sval.equals(NEG) ||
+                    st.sval.equals(LOG)) {
 
                 result = unary();
-                }
-             
-            else if(this.st.sval.equals("if") ||
+            }
+
+            else if (this.st.sval.equals("if") ||
                     this.st.sval.equals("else")) {
                 result = conditional();
             } else {
@@ -265,6 +280,7 @@ public class CalculatorParser {
     /**
      * Checks what type of Unary operation the token read is and
      * then continues with the expression that the operator holds
+     * 
      * @return a SymbolicExpression to be evaluated
      * @throws IOException by nextToken() if it reads invalid input
      */
@@ -289,15 +305,15 @@ public class CalculatorParser {
 
     private String getConditionalOperation() throws IOException {
         this.st.nextToken(); // go to next, now we get our condition.
-        char firstHalfOperation = ((char)st.ttype);
+        char firstHalfOperation = ((char) st.ttype);
         this.st.nextToken();
-        char secondHalfOperation = ((char)st.ttype);
+        char secondHalfOperation = ((char) st.ttype);
 
         var sb = new StringBuilder();
 
         if (firstHalfOperation == '<') {
             sb.append(firstHalfOperation);
-            
+
             if (secondHalfOperation == '=') {
                 // <=
                 sb.append(secondHalfOperation);
@@ -309,14 +325,14 @@ public class CalculatorParser {
             if (secondHalfOperation == '=') {
                 // >=
                 sb.append(secondHalfOperation);
-            }  else {
+            } else {
                 this.st.pushBack();
             }
 
         } else if (firstHalfOperation == '=') {
             sb.append(firstHalfOperation);
             if (secondHalfOperation == '=') {
-                // == 
+                // ==
                 sb.append(secondHalfOperation);
             } else {
                 // wrong syntax..
@@ -327,6 +343,13 @@ public class CalculatorParser {
             throw new SyntaxErrorException("Unexpected symbol.");
         }
 
+        while (this.st.ttype == '<' ||
+                this.st.ttype == '>' ||
+                this.st.ttype == '=') {
+            this.st.nextToken();
+        }
+
+        // TODO for some reason > is not returned when it is the value...
         return sb.toString();
 
     }
@@ -354,11 +377,18 @@ public class CalculatorParser {
         } else {
             throw new SyntaxErrorException("Invalid operation.");
         }
-        this.st.nextToken();
-        this.st.nextToken();
+
+        //
+        while (this.st.ttype != '{') {
+            this.st.nextToken();
+        }
+
         var ifScope = primary();
 
-        this.st.nextToken();
+        while (this.st.ttype != StreamTokenizer.TT_WORD) {
+            this.st.nextToken();
+        }
+
         if (!this.st.sval.equals("else")) {
             throw new SyntaxErrorException("Expected else");
         }
@@ -368,31 +398,17 @@ public class CalculatorParser {
 
         return new Conditional(cond, ifScope, elseScope);
 
-/*      
-        if x < y { 42 } else { 4711 }
-        SymbolicExpression result = term();
-        this.st.nextToken();
-        while (this.st.ttype == ADDITION || this.st.ttype == SUBTRACTION) {
-            int operation = st.ttype;
-            this.st.nextToken();
-            if (operation == ADDITION) {
-                result = new Addition(result, term());
-            } else {
-                result = new Subtraction(result, term());
-            }
-            this.st.nextToken();
-        }
-        this.st.pushBack();
-        return result; */
-
     }
 
     /**
-     * Checks if the token read is a number - should always be a number in this method
+     * Checks if the token read is a number - should always be a number in this
+     * method
+     * 
      * @return a SymbolicExpression to be evaluated
-     * @throws IOException by nextToken() if it reads invalid input
-     * @throws SyntaxErrorException if the token parsed cannot be turned into a valid expression,
-     *         expected a number which is not present
+     * @throws IOException          by nextToken() if it reads invalid input
+     * @throws SyntaxErrorException if the token parsed cannot be turned into a
+     *                              valid expression,
+     *                              expected a number which is not present
      */
     private SymbolicExpression number() throws IOException {
         this.st.nextToken();
